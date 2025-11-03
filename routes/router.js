16016030a940c426bc76c2978e096497598846b8
jpa1994 <john.astvazaturov@mscoding.org>
2025-11-03 11:07:06 -0600

    started on route route and set up error handling

---
+++ routes/router.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const PORT = process.env.PORT || 3000
+
+// Root Route => http://localhost:3000/api
+router.get('/api', (req, res)=> {
+    // res.send('album api')
+    res.json({
+        'All Albums': `http://localhost:${PORT}/api/album`
+    })
+})
+
+// Error handling
+router.use((req, res, next)=> {
+    res.status(404)
+    .send('<h1>404 Error: This page does not exist.</h1>')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Build server
 const express = require('express')
 const server = express()
+const router = require('./routes/router')
 const PORT = process.env.PORT || 3000
 
 // Handle security
@@ -19,9 +20,11 @@ server.use(helmet.contentSecurityPolicy({
     }
 }))
 
-
 server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: true}))
+
+// localhost:3000
+server.use('/', router)
 
 server.listen(PORT, ()=> console.log`The Dodgers have won the 2025 World Series!!!`)
