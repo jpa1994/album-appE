a6f15c6e7dd07d534af050a7c8edb5332a412568
jpa1994 <john.astvazaturov@mscoding.org>
2025-11-03 10:49:11 -0600

    added helmet and cors to server.js; built config/dbconfig.js

--- server.js
+++ server.js
@@ -3,4 +3,25 @@ const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, ()=> console.log`The Dodgers have won the 2025 Series!!!`)
+// Handle security
+const helmet = require('helmet')
+const cors = require('cors')
+
+// configuring helmet
+//server.use(helmet())
+server.use(helmet.contentSecurityPolicy({
+    useDefaults: true,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbeddedPolicy: false,
+    directives: {
+        "img-src": ["'self'", "https: data"],
+        "scriptSrc": ["'self", "cdn.jsdeliver.net"]
+    }
+}))
+
+
+server.use(cors())
+server.use(express.json())
+server.use(express.urlencoded({ extended: true}))
+
+server.listen(PORT, ()=> console.log`The Dodgers have won the 2025 World Series!!!`)
